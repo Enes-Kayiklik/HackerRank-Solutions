0e70bf62249b5cf4c709e6f47c9d51141e85ed3e
Enes Kayiklik <eneskayiklik@gmail.com>
2020-07-24 09:26:11 +0300

    added DataTypes

--- src/thirtydayofcode/DataTypes.java
+++ src/thirtydayofcode/DataTypes.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class DataTypes {
     private static Scanner scan = new Scanner(System.in);
-
     public static void main(String[] args) {
         System.out.println(
                 (Integer.parseInt(scan.nextLine()) + 4) + "\n" +
