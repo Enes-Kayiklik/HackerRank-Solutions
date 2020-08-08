3553276c9b9cd9dd5e31b0ee838bd8695e41fbb2
Enes Kayiklik <eneskayiklik@gmail.com>
2020-08-08 11:39:22 +0300

    added BeautifulTriplets

---
+++ src/problemsolving/BeautifulTriplets.kt
@@ -0,0 +1,17 @@
+package problemsolving
+
+import java.util.*
+
+private val scanner = Scanner(System.`in`)
+fun main() {
+    val size = scanner.nextInt()
+    val difference = scanner.nextInt()
+    var output = 0
+    IntArray(size) { scanner.nextInt() }.also { arr ->
+        arr.forEach {
+            if ((it + difference) in arr && (it + 2 * difference) in arr)
+                output++
+        }
+        print(output)
+    }
+}
