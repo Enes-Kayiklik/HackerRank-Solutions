16adc3232ae1607cb15d5513cc5f1c1878d71b90
Enes Kayiklik <eneskayiklik@gmail.com>
2020-08-08 17:25:23 +0300

    added Pangrams

---
+++ src/problemsolving/Pangrams.kt
@@ -0,0 +1,13 @@
+package problemsolving
+
+import java.util.*
+
+private val scanner = Scanner(System.`in`)
+fun main() {
+    scanner.nextLine().toLowerCase().also { string ->
+        println(
+            if (('a'..'z').all { char -> char in string } && ' ' in string) "pangram"
+            else "not pangram"
+        )
+    }
+}
