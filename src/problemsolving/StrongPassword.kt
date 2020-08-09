a1ac7dde632cbf44b9fd7665591f51b8900ca8cb
Enes Kayiklik <eneskayiklik@gmail.com>
2020-08-09 20:13:13 +0300

    added StrongPassword

--- src/problemsolving/StrongPassword.kt
+++ src/problemsolving/StrongPassword.kt
@@ -1,8 +1,18 @@
 package problemsolving
 
 import java.util.*
+import kotlin.math.max
 
 private val scanner = Scanner(System.`in`)
 fun main() {
-
+    scanner.nextInt().also { size ->
+        var count = 0
+        scanner.next().also { string ->
+            if (string.any { it in 'a'..'z' }) count++
+            if (string.any { it in 'A'..'Z' }) count++
+            if (string.any { it in '0'..'9' }) count++
+            if (string.any { it in "!@#\$%^&*()-+" }) count++
+        }
+        print(max(4 - count, 6 - size))
+    }
 }
